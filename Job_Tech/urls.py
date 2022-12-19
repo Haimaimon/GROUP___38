3b2e9de51f3fd948eaffb5b92e8401511bcc1250
Lidorkatzav <Lidorka@ac.sce.ac.il>
2022-12-19 11:45:13 +0200

    LidorKa
    error fixed

--- Job_Tech/urls.py
+++ Job_Tech/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index, name='index'),
     path('register/', views.register_user, name='register'),
-    path('index/register',views.regi, name='regi'),
     path('login/', views.loginPage, name="login"),
     path('', views.home, name="home"),
 ]
